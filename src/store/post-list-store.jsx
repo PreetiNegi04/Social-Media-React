e4beb56c4c52262d0d59a97baf316770abac8041
Preeti <negipreeti2003@gmail.com>
2025-01-13 19:27:27 +0530

    commit

--- src/store/post-list-store.jsx
+++ src/store/post-list-store.jsx
@@ -1,36 +1,65 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
-const PostList = createContext({
+export const PostList = createContext({
   postList: [],
   addPost: () => {},
   deletePost: () => {}
 });
 
-const addPost = () => {};
-const deletePost = () => {};
-
 const postListReducer = (currentList, action) => {
-  return currentList;
+  let newPostList = currentList;
+  if(action.type === "DELETE_POST"){
+    newPostList = currentList.filter(
+      post => post.id !== action.payload.postId);
+  }
+  else if(action.type === "ADD_POST"){
+    newPostList = [action.payload, ...currentList];
+  }
+  return newPostList;
 }
 const PostListProvider = ({children}) => {
-  const [postList, dipatchPostList] = useReducer(
-    postListReducer,[]);
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POST_LIST
+  );
 
-  return <PostList.Provider value={{
+  const addPost = (userId, postTitle, postContent, reaction, tags) => {
+    dispatchPostList({
+      type: "ADD_POST",
+      payload :{
+        id : Date.now(),
+        userId: userId,
+        title: postTitle,
+        body: postContent,
+        reactions: reaction,
+        tags: tags
+      }
+    })
+  };
+  const deletePost = (postId) => {
+    dispatchPostList({
+      type: "DELETE_POST",
+      payload: {
+        postId,
+      },
+    });
+  };
+  return (
+  <PostList.Provider value={{
     postList: postList,
     addPost: addPost,
     deletePost: deletePost
   }}
-  >{children}</PostList.Provider>;
+  >{children}</PostList.Provider>);
 }
 
 const DEFAULT_POST_LIST = [{
   id : '1',
-  title: '',
-  body: '',
-  reactions : 0,
-  userId: '',
-  tags: []
+  title: 'Going to Mumbai',
+  body: 'Leaving to Mumbai this evening, will have so much fun',
+  reactions : 5,
+  userId: 'user-8',
+  tags: ['vacation', 'mumbai', 'enjoying']
 }];
  
 export default PostListProvider;
