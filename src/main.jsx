4df261eeae6cae91e397c4d4f1fb0e42049a1da3
Preeti <negipreeti2003@gmail.com>
2025-01-17 19:20:19 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import CreatePost, { createPostAction } from "./components/CreatePost.jsx";
 import PostList, { postLoader } from './components/PostList.jsx';
 const router = createBrowserRouter([
   {path : "/", element : <App/>, children:[
-    {path : "/", element : <PostList/>, loader : postLoader},
+    {path : "/", element : <PostList/>, loader : postLoader}, 
     {path : "/create-post", element : <CreatePost/>, action : createPostAction}
   ]},
 ]);
